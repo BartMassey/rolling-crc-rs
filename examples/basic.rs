ab2042ad5ccb62c77d5523ceab22644150e33d60
Bart Massey <bart@cs.pdx.edu>
2018-05-27 11:50:10 -0700

    added basic example; doesn't work yet

---
+++ examples/basic.rs
@@ -0,0 +1,18 @@
+extern crate rolling_crc;
+use rolling_crc::*;
+
+fn main() {
+    let context = RollingCRCContext::new(3);
+    let mut roll_crc = RollingCRC::new(&context);
+    let bytes = "hello world".as_bytes();
+    for i in 0..bytes.len() {
+        let crc = roll_crc.push(bytes[i]);
+        if i < 2 {
+            println!("{} {:?}", i, crc);
+        } else {
+            let true_crc = context.crc(&bytes[i-2..=i]);
+            let crc = crc.unwrap();
+            println!("{} {:08x?} {:08x?}", i, crc, true_crc);
+        }
+    }
+}
